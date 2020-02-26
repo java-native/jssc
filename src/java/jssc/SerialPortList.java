7159342a9ec4f213107d658767a64534fbeca17b
Dmitry Cherkas <cherkas.da@gmail.com>
2020-02-26 21:31:06 +0300

    patch for Arduino by Cristian Maglie (https://github.com/arduino/Arduino/commit/f91670e0d039a237ecf73ad902bd5a487308ea64)

--- src/java/jssc/SerialPortList.java
+++ src/java/jssc/SerialPortList.java
@@ -53,7 +53,7 @@ public class SerialPortList {
                 break;
             }
             case SerialNativeInterface.OS_MAC_OS_X: {
-                PORTNAMES_REGEXP = Pattern.compile("tty.(serial|usbserial|usbmodem).*");
+                PORTNAMES_REGEXP = Pattern.compile("(tty|cu)\\..*");
                 PORTNAMES_PATH = "/dev/";
                 break;
             }
@@ -155,7 +155,7 @@ public class SerialPortList {
         }
     };
     //<-since 2.1.0
-    
+
     /**
      * Get sorted array of serial ports in the system using default settings:<br>
      *
@@ -199,7 +199,7 @@ public class SerialPortList {
      * Get sorted array of serial ports in the system matched pattern
      *
      * @param pattern RegExp pattern for matching port names <b>(not null)</b>
-     * 
+     *
      * @return String array. If there is no ports in the system String[]
      *
      * @since 2.3.0
@@ -330,12 +330,15 @@ public class SerialPortList {
                     String fileName = file.getName();
                     if(!file.isDirectory() && !file.isFile() && pattern.matcher(fileName).find()){
                         String portName = searchPath + fileName;
-                        long portHandle = serialInterface.openPort(portName, false);//Open port without TIOCEXCL
-                        if(portHandle < 0 && portHandle != SerialNativeInterface.ERR_PORT_BUSY){
-                            continue;
-                        }
-                        else if(portHandle != SerialNativeInterface.ERR_PORT_BUSY) {
-                            serialInterface.closePort(portHandle);
+                        // For linux ttyS0..31 serial ports check existence by opening each of them
+                        if (fileName.startsWith("ttyS")) {
+	                        long portHandle = serialInterface.openPort(portName, false);//Open port without TIOCEXCL
+	                        if(portHandle < 0 && portHandle != SerialNativeInterface.ERR_PORT_BUSY){
+	                            continue;
+	                        }
+	                        else if(portHandle != SerialNativeInterface.ERR_PORT_BUSY) {
+	                            serialInterface.closePort(portHandle);
+	                        }
                         }
                         portsTree.add(portName);
                     }
