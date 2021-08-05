01c8095683b67febf8247413e38e13a3e4e99c00
tresf <tres.finocchiaro@gmail.com>
2021-08-04 23:47:30 -0400

    Move comments to before class

--- src/test/java/jssc/bootpath/ManualBootLibraryPathFailedTest.java
+++ src/test/java/jssc/bootpath/ManualBootLibraryPathFailedTest.java
@@ -6,16 +6,19 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+/**
+ * Tests if a valid <code>jssc.boot.library.path</code> which does NOT contain a native library
+ * will predictably fail.  This test can be run regardless of whether or not a native binary was
+ * created during the build process.
+ *
+ * TODO: This MUST be in its own class to run in a separate JVM (https://stackoverflow.com/questions/68657855)
+ * - JUnit does NOT currently offer JVM unloading between methods.
+ * - maven-surefire-plugin DOES offer JVM unloading between classes using <code>reuseForks=false</code>
+ * - Unloading is needed due to NativeLoader.loadLibrary(...) calls System.loadLibrary(...) which is static
+ */
 public class ManualBootLibraryPathFailedTest {
     @Test
     public void testBootPathOverride() {
-        /**
-         * This must be in its own class to run in a separate JVM
-         * - See also: https://stackoverflow.com/questions/68657855
-         * - NativeLoader.loadLibrary(...) calls System.loadLibrary(...) which is static
-         * - maven-surefire-plugin must be configured with reuseForks=false to use a new JVM for each class
-         * - TODO: If JUnit adds JVM unloading between methods, this class can be consolidated
-         */
         String nativeLibDir = "/"; // This should be valid on all platforms
         System.setProperty("jssc.boot.library.path", nativeLibDir);
         try {
--- src/test/java/jssc/bootpath/ManualBootLibraryPathTest.java
+++ src/test/java/jssc/bootpath/ManualBootLibraryPathTest.java
@@ -8,16 +8,19 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
+/**
+ * Tests if a valid <code>jssc.boot.library.path</code> which DOES contain a native library
+ * will predictably pass.  This test can ONLY be run regardless if a native binary was created
+ * during the build process.  See also <code>maven.exclude.tests</code>.
+ *
+ * TODO: This MUST be in its own class to run in a separate JVM (https://stackoverflow.com/questions/68657855)
+ * - JUnit does NOT currently offer JVM unloading between methods.
+ * - maven-surefire-plugin DOES offer JVM unloading between classes using <code>reuseForks=false</code>
+ * - Unloading is needed due to NativeLoader.loadLibrary(...) calls System.loadLibrary(...) which is static
+ */
 public class ManualBootLibraryPathTest {
     @Test
     public void testBootPathOverride() {
-        /**
-         * This must be in its own class to run in a separate JVM
-         * - See also: https://stackoverflow.com/questions/68657855
-         * - NativeLoader.loadLibrary(...) calls System.loadLibrary(...) which is static
-         * - maven-surefire-plugin must be configured with reuseForks=false to use a new JVM for each class
-         * - TODO: If JUnit adds JVM unloading between methods, this class can be consolidated
-         */
         String nativeLibDir = NativeLibraryUtil.getPlatformLibraryPath(System.getProperty("user.dir") + "/target/cmake/natives/");
         System.setProperty("jssc.boot.library.path", nativeLibDir);
         try {
