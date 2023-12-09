ffc296bdfb935a4589e47c69f06802d1d0987954
Mateusz Pietryga <pietryga.m@gmail.com>
2023-12-09 23:57:04 +0100

    Maven-like Log4j configuration for test execution

--- src/test/java/jssc/junit/rules/DisplayMethodNameRule.java
+++ src/test/java/jssc/junit/rules/DisplayMethodNameRule.java
@@ -2,9 +2,10 @@ package jssc.junit.rules;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.MarkerManager;
 import org.junit.Rule;
-import org.junit.rules.*;
-import org.junit.runner.*;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 
 /**
  * Adds the method name to the JUnit logs, useful for debugging
@@ -15,7 +16,7 @@ public class DisplayMethodNameRule {
         @Override
         protected void starting(Description description) {
             Logger log = LogManager.getLogger(description.getTestClass());
-            log.debug(description.getMethodName());
+            log.info(MarkerManager.getMarker("MethodName"), description.getMethodName());
         }
     };
 }
